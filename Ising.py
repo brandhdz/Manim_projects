b448be58c2b9f86646c14b89a9dfe056f0cf8159
brandhdz <abrandhdz@gmail.com>
2024-11-28 19:26:02 -0600

    Ising grid with color dots

--- Ising.py
+++ Ising.py
@@ -18,11 +18,18 @@ class Ising_grid(Scene):
             r = np.random.rand()
             if r < 0.5:
                 color = RED
+                arr = "UP"
             else:
                 color = BLUE
-            dot = Dot(np.array([np.random.choice(x_a),
-                                np.random.choice(x_a), 0]),
-                               radius = 0.08,
-                               color = color)
+                arr = "DOWN"
+            x_r = np.random.choice(x_a)
+            y_r = np.random.choice(x_a)
+            xy_p = Text(f"({x_r},{y_r})")
+            xy_arr = Text(f"{arr}", color = color)
+            xy_p.to_corner(UP + LEFT)
+            xy_arr.to_corner(2.5*UP + LEFT).scale(0.8)
+            dot = Dot(np.array([x_r, y_r, 0]), radius = 0.08, color = color)
             self.add(dot)
-            self.wait(0.5)
+            self.add(xy_p, xy_arr)
+            self.wait(0.8)
+            self.remove(xy_p, xy_arr)
