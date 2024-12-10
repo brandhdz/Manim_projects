523d22c0f82c30086098e6a507c0b15012c9578f
brandhdz <abrandhdz@gmail.com>
2024-12-10 17:47:34 -0600

    random choice animation

--- Ising.py
+++ Ising.py
@@ -33,3 +33,45 @@ class Ising_grid(Scene):
             self.add(xy_p, xy_arr)
             self.wait(0.8)
             self.remove(xy_p, xy_arr)
+
+class rand_choice(Scene):
+    def construct(self):
+        samples = 5
+        r_l = np.random.rand(samples)
+
+        for r in r_l:
+            r_n = MathTex("%.5f" % r)
+            self.add(r_n)
+            self.wait(0.2)
+            self.remove(r_n)
+            self.wait(0.2)
+
+        r_0 = "0.68348"
+        r_choice = MathTex(f"{r_0}")
+        self.add(r_choice)
+        self.wait(0.5)
+
+        exp = "e^{\\Delta S}"
+        eq1 = MathTex(f"{exp}")
+        eq1.shift(UP)
+        self.play(FadeIn(eq1))
+        self.wait(0.5)
+
+        terms = [r_0, "<" , exp]
+        
+        eq2 = MathTex(r_0, "<", exp, substrings_to_isolate = terms)
+        eq2.set_color_by_tex("<", RED)
+
+        self.play(TransformMatchingShapes(r_choice, eq2[0]),
+                  TransformMatchingShapes(eq1, eq2[2]))
+        self.wait(0.5)
+
+        self.add(eq2)
+        self.wait(0.5)
+
+        #dot = Dot(point = ORIGIN, radius = 0.08, color = YELLOW)
+        #self.add(dot)
+        #self.wait(0.5) 
+
+        #self.add(MathTex("<", color = RED).shift(ORIGIN))
+        #self.wait(0.5)
