cebeb5363a7f27a9cf99f3a1961625095c3f2544
brandhdz <abrandhdz@gmail.com>
2025-01-24 11:45:19 -0600

    Ising grid animation with arrows

--- Ising.py
+++ Ising.py
@@ -75,3 +75,41 @@ class rand_choice(Scene):
 
         #self.add(MathTex("<", color = RED).shift(ORIGIN))
         #self.wait(0.5)
+
+class grid_arrow(Scene):
+    def construct(self):
+        x_a = np.arange(-3, 4)
+        for x in x_a:
+            for y in  x_a:
+                dot = Dot(np.array([x, y, 0]) , radius = 0.08)
+                self.add(dot)
+
+        dot = Dot(point = ORIGIN, radius = 0.08, color = YELLOW)
+        self.add(dot)
+        self.wait(0.5)
+
+        samples = 10
+        for s in range(samples):
+            x_r = np.random.choice(x_a)
+            y_r = np.random.choice(x_a)
+            
+            r = np.random.rand()
+            if r < 0.5:
+                color = RED
+                arr = "UP"
+                y_r_d = y_r + 0.5
+            else:
+                color = BLUE
+                arr = "DOWN"
+                y_r_d = y_r - 0.5
+                
+            xy_p = Text(f"({x_r},{y_r})")
+            xy_arr = Text(f"{arr}", color = color)
+            xy_p.to_corner(UP + LEFT)
+            xy_arr.to_corner(2.5*UP + LEFT).scale(0.8)
+            arr = Arrow(start = [x_r, y_r, 0], end = [x_r, y_r_d, 0],
+                        max_stroke_width_to_length_ratio = 55, buff = 3, color = color)
+            self.add(arr)
+            self.add(xy_p, xy_arr)
+            self.wait(0.8)
+            self.remove(xy_p, xy_arr)
